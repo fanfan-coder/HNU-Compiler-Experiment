b7177a7209f078bc2ea56acc75b643f11eed99a1
ljkjk <53215232+ljkjk@users.noreply.github.com>
2019-11-19 20:48:51 +0800

    Add files via upload

---
+++ EXP2_SCAN/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include "scan.h"
+using namespace std;
+
+int main()
+{
+    freopen("SAMPLE_r.TNY", "r", stdin);
+    TokenType token;
+    while(!EOFFlag && (token = getToken()) != ERROR) {}
+    return 0;
+}
